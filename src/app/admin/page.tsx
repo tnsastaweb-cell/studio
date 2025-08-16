5433aae9fef9e9779325af79a6fdcd6589a477b9
sastaweb <tnsastaweb@gmail.com>
2025-08-16 14:59:47 +0000

    Runtime Error
    Error: React is not defined
    
    src/app/admin/page.tsx (96:

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
