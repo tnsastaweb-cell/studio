392320d2213dc998652ec1f8df83ffb715870948
sastaweb <tnsastaweb@gmail.com>
2025-08-19 02:52:13 +0000

    KALLAKURICHI CHENGALPATTU RANIPET TIRUPATHUR

--- src/services/panchayats.ts
+++ src/services/panchayats.ts
@@ -18,6 +18,7 @@ import { DHARMAPURI_PANCHAYATS } from './panchayat-data/dharmapuri';
 import { ERODE_PANCHAYATS } from './panchayat-data/erode';
 import { THE_NILGIRIS_PANCHAYATS } from './panchayat-data/the-nilgiris';
 import { COIMBATORE_PANCHAYATS } from './panchayat-data/coimbatore';
+import { TIRUPPUR_PANCHAYATS } from './panchayat-data/tiruppur';
 import { THANJAVUR_PANCHAYATS } from './panchayat-data/thanjavur';
 import { NAGAPATTINAM_PANCHAYATS } from './panchayat-data/nagapattinam';
 import { MAYILADUTHURAI_PANCHAYATS } from './panchayat-data/mayiladuthurai';
@@ -25,6 +26,7 @@ import { TIRUVARUR_PANCHAYATS } from './panchayat-data/tiruvarur';
 import { TIRUCHIRAPPALLI_PANCHAYATS } from './panchayat-data/tiruchirappalli';
 import { KARUR_PANCHAYATS } from './panchayat-data/karur';
 import { PERAMBALUR_PANCHAYATS } from './panchayat-data/perambalur';
+import { ARIYALUR_PANCHAYATS } from './panchayat-data/ariyalur';
 import { PUDUKKOTTAI_PANCHAYATS } from './panchayat-data/pudukkottai';
 import { MADURAI_PANCHAYATS } from './panchayat-data/madurai';
 import { THENI_PANCHAYATS } from './panchayat-data/theni';
@@ -36,6 +38,10 @@ import { TIRUNELVELI_PANCHAYATS } from './panchayat-data/tirunelveli';
 import { THOOTHUKKUDI_PANCHAYATS } from './panchayat-data/thoothukkudi';
 import { KANNIYAKUMARI_PANCHAYATS } from './panchayat-data/kanniyakumari';
 import { KRISHNAGIRI_PANCHAYATS } from './panchayat-data/krishnagiri';
+import { KALLAKURICHI_PANCHAYATS } from './panchayat-data/kallakurichi';
+import { CHENGALPATTU_PANCHAYATS } from './panchayat-data/chengalpattu';
+import { RANIPET_PANCHAYATS } from './panchayat-data/ranipet';
+import { TIRUPATHUR_PANCHAYATS } from './panchayat-data/tirupathur';
 
 export type { Panchayat };
 
@@ -52,6 +58,7 @@ export const MOCK_PANCHAYATS: Panchayat[] = [
   ...ERODE_PANCHAYATS,
   ...THE_NILGIRIS_PANCHAYATS,
   ...COIMBATORE_PANCHAYATS,
+  ...TIRUPPUR_PANCHAYATS,
   ...THANJAVUR_PANCHAYATS,
   ...NAGAPATTINAM_PANCHAYATS,
   ...MAYILADUTHURAI_PANCHAYATS,
@@ -59,6 +66,7 @@ export const MOCK_PANCHAYATS: Panchayat[] = [
   ...TIRUCHIRAPPALLI_PANCHAYATS,
   ...KARUR_PANCHAYATS,
   ...PERAMBALUR_PANCHAYATS,
+  ...ARIYALUR_PANCHAYATS,
   ...PUDUKKOTTAI_PANCHAYATS,
   ...MADURAI_PANCHAYATS,
   ...THENI_PANCHAYATS,
@@ -70,8 +78,8 @@ export const MOCK_PANCHAYATS: Panchayat[] = [
   ...THOOTHUKKUDI_PANCHAYATS,
   ...KANNIYAKUMARI_PANCHAYATS,
   ...KRISHNAGIRI_PANCHAYATS,
+  ...KALLAKURICHI_PANCHAYATS,
+  ...CHENGALPATTU_PANCHAYATS,
+  ...RANIPET_PANCHAYATS,
+  ...TIRUPATHUR_PANCHAYATS,
 ];
-
-    
-
-    
